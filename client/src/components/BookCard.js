3005831047747104e645e2c94f8ca909cad50b98
maddox earles <mdxear@gmail.com>
2023-08-10 10:40:34 -0600

    delete button finished

--- client/src/components/BookCard.js
+++ client/src/components/BookCard.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function BookCard({ book, handleClick }){
+function BookCard({ book, handleClick, deleteBook }){
 
     return(
         <div className="ui raised card">
@@ -9,9 +9,11 @@ function BookCard({ book, handleClick }){
                 <div className="description">Author: {book.author}</div>
             </div>
             <div className="extra content">
-                <div className="ui buttons">
-                    <div className="ui basic button" 
+                <div className="ui basic two buttons">
+                    <div className="ui basic blue button" 
                     onClick={() => handleClick(book)}>Add to Calendar</div>
+                    <div className="ui basic red button" 
+                    onClick={() => deleteBook(book)}>Delete Book</div>
                 </div>
             </div>
         </div>
